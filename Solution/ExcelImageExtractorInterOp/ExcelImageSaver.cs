daa5e38d4fb68a28c0a54dd3b87935d34bc77280
Michele Fosco <michele.fosco@gmail.com>
2025-10-24 09:17:40 +0200

    Versione ottimizzata della libreria che usa Interops

--- Solution/ExcelImageExtractorInterOp/ExcelImageSaver.cs
+++ Solution/ExcelImageExtractorInterOp/ExcelImageSaver.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Threading;
 using System.Windows.Forms; // Per Clipboard
 
 
@@ -10,108 +9,80 @@ namespace ExcelImageExtractorInterOp
 {
     public class ExcelImageSaver
     {
-
-        string _excelFilePath;
+        readonly Microsoft.Office.Interop.Excel.Application excelApp = null;
+        readonly Workbook workbook = null;
+        Worksheet worksheet = null;
 
         public ExcelImageSaver(string excelFilePath)
         {
-            _excelFilePath = excelFilePath;
-        }
-
-        public void Close()
-        {
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            excelApp = new Microsoft.Office.Interop.Excel.Application();
+            excelApp.Visible = false;
+            workbook = excelApp.Workbooks.Open(excelFilePath);
         }
 
         public void ExportImages(string workSheetName, string rangeAddress, string destinationPath)
         {
-            Microsoft.Office.Interop.Excel.Application excelApp = null;
-            Workbook workbook = null;
-            Worksheet worksheet = null;
             Range range = null;
             try
             {
-                excelApp = new Microsoft.Office.Interop.Excel.Application();
-                excelApp.Visible = false;
-                workbook = excelApp.Workbooks.Open(_excelFilePath);
+                // seleziono il foglio
                 worksheet = workbook.Sheets[workSheetName];
 
-                //  string rangeAddress = $"{startRange}:{endRange}";
+                // seleziono il range
                 range = worksheet.Range[rangeAddress];
 
+                // copio il range come immmagine nella Clipboard
                 range.CopyPicture(XlPictureAppearance.xlScreen, XlCopyPictureFormat.xlBitmap);
 
+                // Per funzionare questo codice deve essere eseguito nel thread principale dell'applicazione
                 // Recupera l'immagine dagli appunti
-                // Per funzionare questo codice non deve essere eseguito in un thread separato 
                 Image clipboardImage = null;
                 if (Clipboard.ContainsImage())
                 {
                     clipboardImage = Clipboard.GetImage();
-                }
-                else
-                {
-                    throw new Exception($"The clipboard non contiene un'immagine come previsto per il file '{destinationPath}'");
+                    if (clipboardImage != null)
+                    { clipboardImage.Save(destinationPath, ImageFormat.Png); }
                 }
 
-                if (clipboardImage != null)
-                {
-                    clipboardImage.Save(destinationPath, ImageFormat.Png);
-                }
-
-
-                //Ok con UnitTest no con Windows
-                //if (System.Windows.Forms.Clipboard.ContainsImage())
-                //{
-                //    var img = System.Windows.Forms.Clipboard.GetImage();
-                //    img.Save(destinationPath, ImageFormat.Png);
-                //}
-                //else
-                //{
-                //    throw new Exception($"Clipboard vuota per il file immagine da generare '{destinationPath}'");
-                //}
-
-                #region OK, funziona tramite gli unit tests
-                // Clipboard access requires STA thread, use a new thread
-                //Thread thread = new Thread(() =>
-                //{
-                //    if (System.Windows.Forms.Clipboard.ContainsImage())
-                //    {
-                //        var img = System.Windows.Forms.Clipboard.GetImage();
-                //        img.Save(destinationPath, ImageFormat.Png);
-                //    }
-                //    else
-                //    {
-                //        throw new Exception($"Clipboard vuota per il file immagine da generare '{destinationPath}'");
-                //    }
-                //});
-
-                //thread.SetApartmentState(ApartmentState.STA);
-                //thread.Start();
-                //thread.Join();
-                #endregion
-
-                workbook.Close(false);
-                excelApp.Quit();
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(range);
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                // Segnalo se l'estrazione dell'immagine non è andata a buon fine
+                if (clipboardImage == null)
+                { throw new Exception($"la Clipboard non contiene un'immagine come previsto per il file '{destinationPath}'"); }
             }
             catch (Exception ex)
             {
+                //todo rimuovere dopo i test
                 var a = ex.Message;
                 var b = ex.InnerException;
             }
             finally
             {
-                if (range != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(range);
-                if (worksheet != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
-                if (workbook != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-                if (excelApp != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                if (range != null)
+                { System.Runtime.InteropServices.Marshal.ReleaseComObject(range); }
+
+                if (worksheet != null)
+                { System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet); }
+
+            }
+        }
+
+        public void Close()
+        {
+            if (worksheet != null)
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
             }
 
+            if (workbook != null)
+            {
+                workbook.Close(false);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            }
+
+            if (excelApp != null)
+            {
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
         }
     }
 }
