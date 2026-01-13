9e8af18177231d8f7b41cfb9502d9a7f48ebc55d
Michele Fosco <michele.fosco@gmail.com>
2026-01-13 19:13:07 +0100

    Waiting form con icona

--- Solution/PptGeneratorGUI/frmWaiting.Designer.cs
+++ Solution/PptGeneratorGUI/frmWaiting.Designer.cs
@@ -36,7 +36,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(167, 11);
-            this.ControlBox = false;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
